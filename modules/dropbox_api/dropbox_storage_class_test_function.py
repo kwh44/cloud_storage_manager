18e8e38447a3fb00c232eb0fb54c323720bfd15a
Andrii Maistruk <andrii.maistruk@gmail.com>
2018-05-26 13:41:59 +0300

    googledrive api v2/v3 updown scripts added

--- modules/dropbox_api/dropbox_storage_class_test_function.py
+++ modules/dropbox_api/dropbox_storage_class_test_function.py
@@ -3,7 +3,7 @@ from dropbox_storage_class import DropBoxStorage
 def class_test():
     d = DropBoxStorage()
     d.link_account()
-
+    print(d.dbx_user_account)
 
 if __name__ == "__main__":
     class_test()
